d6431105b36dde84ce0aa3ce90787fb938c90687
abhishekgowdak1997 <86243166+abhishekgowdak1997@users.noreply.github.com>
2021-07-08 10:48:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './App.css';
+import Vote from './components/Vote';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Vote/>
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
